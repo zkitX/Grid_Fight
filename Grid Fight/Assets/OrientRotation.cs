12fc0e14380791d680f69df9aa248766ed6dbdef
devplaytra <dev@playtra.com>
2019-11-29 15:27:34 +0000

    ACE - Fixed Particles

---
+++ Grid Fight/Assets/OrientRotation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrientRotation : MonoBehaviour
+{
+
+
+    private Vector2 previousPos;
+    private Vector2 currentPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        previousPos = currentPos;
+        currentPos = transform.position;
+        //transform.RotateAround(currentPos,new Vector3(0,0,1),)
+             transform.rotation = Quaternion.FromToRotation(new Vector3(0, 0, 1), previousPos - currentPos);
+        var main = GetComponent<ParticleSystem>().main;
+        main.startRotationZMultiplier = Quaternion.ToEulerAngles( Quaternion.FromToRotation(new Vector3(0, 0, 1), previousPos - currentPos)).z ;
+    }
+}
---
+++ Grid Fight/Assets/Scripts/SteadyRotation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SteadyRotation : MonoBehaviour
+{
+
+    public Vector3 Rotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.rotation = Quaternion.Euler(Rotation);
+    }
+}
