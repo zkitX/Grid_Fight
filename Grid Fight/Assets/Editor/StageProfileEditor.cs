cb4325fc89a9ccb70ed212b85d7074c0d9566e2c
Beltain Jordaan <Belt@playtra.com>
2020-03-05 17:16:23 +0000

    Belt - Parity Push for Dan

---
+++ Grid Fight/Assets/Editor/StageProfileEditor.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(StageProfile))]
+public class StageProfileEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        StageProfile origin = (StageProfile)target;
+
+        EditorGUILayout.Space();
+        EditorGUILayout.Separator();
+        EditorGUILayout.LabelField("CHARACTERS:");
+        foreach (CharacterBaseInfoClass charInfoClass in origin.BattleInfoManager.GetComponent<BattleInfoManagerScript>().PlayerBattleInfo)
+        {
+            EditorGUILayout.LabelField(charInfoClass.Name.ToString() + " || " + charInfoClass.CharacterName.ToString());
+        }
+
+        EditorGUILayout.Space();
+        string theTypeOfStage = "";
+        switch (origin.BattleInfoManager.GetComponent<BattleInfoManagerScript>().MatchInfoType)
+        {
+            case (MatchType.PPvE):
+                theTypeOfStage = "Co-op";
+                break;
+            case (MatchType.PPvPP):
+                theTypeOfStage = "Team Deathmatch";
+                break;
+            case (MatchType.PvE):
+                theTypeOfStage = "Player vs Enemy";
+                break;
+            case (MatchType.PvP):
+                break;
+            default:
+                theTypeOfStage = origin.BattleInfoManager.GetComponent<BattleInfoManagerScript>().MatchInfoType.ToString();
+                break;
+        }
+        EditorGUILayout.LabelField("BATTLE TYPE: " + theTypeOfStage);
+
+
+
+    }
+}
--- Grid Fight/Assets/Scripts/SceneManagers/SceneCreation/StageProfile.cs
+++ Grid Fight/Assets/Scripts/SceneManagers/SceneCreation/StageProfile.cs
@@ -17,15 +17,4 @@ public class StageProfile : ScriptableObject
     [SerializeField] public GameObject EventManager;
     [SerializeField] public GameObject Wave;
 
-    StageProfile()
-    {
-       /* if (Rewired == null) Rewired = null;
-        if (Rewired == null) BattleInfoManager = null;
-        if (Rewired == null) AudioManager = null;
-        if (Rewired == null) BattleManager = null;
-        if (Rewired == null) BaseEnvironment = null;
-        if (Rewired == null) UI_Battle = null;
-        if (Rewired == null) Wave = null;
-        if (Rewired == null) EventManager = Resources.Load("Default_Stage_File").;*/
-    }
 }
