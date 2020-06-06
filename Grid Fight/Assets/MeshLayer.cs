39ddd465fe8d5165eba216b54f837cfe18a1b943
devplaytra <dev@playtra.com>
2020-06-06 17:06:33 +0100

    ACE - first waterfall optimized

---
+++ Grid Fight/Assets/MeshLayer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshLayer : MonoBehaviour
+{
+    public int Layer = 305;
+
+    private void OnEnable()
+    {
+        gameObject.GetComponent<MeshRenderer>().sortingOrder = Layer;
+    }
+}
