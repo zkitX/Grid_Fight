b80332974742cfcc2fa10fd1f126e5fc571e2a0f
dev <dev@playtra.com>
2020-06-18 09:55:12 +0100

    Riki - callrecruitchar test Vini

---
+++ Grid Fight/Assets/Scripts/FungusScripts/Commands/CallRecruitChar.cs
@@ -0,0 +1,30 @@
+﻿using Fungus;
+using MyBox;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CommandInfo("Scripting",
+                "Call CallRecruitChar",
+                "CallRecruitChar")]
+[AddComponentMenu("")]
+public class CallRecruitChar : Command
+{
+
+    public CharacterNameType CharacterId;
+
+    #region Public members
+
+    public override void OnEnter()
+    {
+        BattleManagerScript.Instance.RecruitCharFromWave(CharacterId);
+    }
+
+    
+    public override Color GetButtonColor()
+    {
+        return new Color32(235, 191, 217, 255);
+    }
+
+    #endregion
+}
