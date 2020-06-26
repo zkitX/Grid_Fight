a623ce65e715e6ce416e87db46883aae599ed786
dev <dev@playtra.com>
2020-06-26 14:56:26 +0100

    Riki - call change tile status

---
+++ Grid Fight/Assets/Scripts/FungusScripts/Commands/CallChangeTileStatus.cs
@@ -0,0 +1,33 @@
+﻿using Fungus;
+using MyBox;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CommandInfo("Scripting",
+                "Call CallChangeTileStatus",
+                "CharInfo")]
+[AddComponentMenu("")]
+public class CallChangeTileStatus : Command
+{
+
+    public Vector2Int Pos;
+    public BattleTileStateType State;
+
+    #region Public members
+
+    public override void OnEnter()
+    {
+        GridManagerScript.Instance.SetBattleTileState(Pos, State);
+        Continue();
+    }
+
+   
+
+    public override Color GetButtonColor()
+    {
+        return new Color32(235, 191, 217, 255);
+    }
+
+    #endregion
+}
