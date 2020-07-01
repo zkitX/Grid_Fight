406bf6344632e8379645bfad539ae3dc342ba5da
devplaytra <dev@playtra.com>
2020-07-01 11:16:57 +0100

    ACE - Orient Rotation Script Update

--- Grid Fight/Assets/Scripts/VFX/OrientRotation.cs
+++ Grid Fight/Assets/Scripts/VFX/OrientRotation.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class OrientRotation : MonoBehaviour
 {
 
-    public Vector3 adjustment = new Vector3();
+    public Vector3 AxisOfRotation = new Vector3(0,0,1);
+    public float Adjustment = 180;
     private Vector2 previousPos;
     private Vector2 currentPos;
 
@@ -14,14 +15,18 @@ public class OrientRotation : MonoBehaviour
     void LateUpdate()
     {
         //Check if the object is still for no reasons
-        if((Vector2)transform.position!=previousPos)
+        if((Vector2)transform.position!= currentPos)
             //bake previous position to take a new one
             previousPos = currentPos;
         currentPos = transform.position;
         //transform.RotateAround(currentPos,new Vector3(0,0,1),)
-        transform.rotation = Quaternion.FromToRotation(new Vector3(0, 0, 1),(Vector3) previousPos - (Vector3)currentPos + adjustment);
+        var dir = currentPos - previousPos;
+        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle+Adjustment, AxisOfRotation);
+        //transform.rotation = Quaternion.FromToRotation(new Vector3(0, 0, 1),(Vector3) previousPos - (Vector3)currentPos + adjustment);
         var main = GetComponent<ParticleSystem>().main;
         int Offset = transform.localScale.x < 0 ? 1 : 0;
-        main.startRotationZMultiplier = Quaternion.ToEulerAngles( Quaternion.FromToRotation(new Vector3(0, 0, 1), previousPos - currentPos)).z+180*(Offset) ;
+        //main.startRotationZMultiplier = Quaternion.ToEulerAngles( Quaternion.FromToRotation(new Vector3(0, 0, 1), previousPos - currentPos)).z+180*(Offset) ;
+        main.startRotationZMultiplier = angle;
     }
 }
