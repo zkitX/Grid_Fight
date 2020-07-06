88e4421782168bdb012611ebe6a9f5472209ea06
devplaytra <dev@playtra.com>
2020-07-06 18:17:14 +0100

    ACE - waterfall optimized

---
+++ Grid Fight/Assets/EnableOnStartupScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableOnStartupScript : MonoBehaviour
+{
+    public List<GameObject> ObjectsToEnable;
+    private void Awake()
+    {
+        foreach(GameObject g in ObjectsToEnable)
+        {
+            g.SetActive(true);
+        }
+    }
+}
