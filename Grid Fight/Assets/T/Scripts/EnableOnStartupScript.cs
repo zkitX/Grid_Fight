78264a23eb5e7b9665c8de92aee13f4903f9f0a1
devplaytra <dev@playtra.com>
2020-07-20 09:51:18 +0100

    ACE - Boss stage 02 Particle System

--- Grid Fight/Assets/T/Scripts/EnableOnStartupScript.cs
+++ Grid Fight/Assets/T/Scripts/EnableOnStartupScript.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class EnableOnStartupScript : MonoBehaviour
 {
     public List<GameObject> ObjectsToEnable;
+    public List<GameObject> ObjectsToDisable;
     private void Awake()
     {
-        foreach(GameObject g in ObjectsToEnable)
+        foreach (GameObject g in ObjectsToEnable)
         {
             g.SetActive(true);
+        }
+        foreach (GameObject g in ObjectsToDisable)
+        {
+            g.SetActive(false);
         }
     }
 }
