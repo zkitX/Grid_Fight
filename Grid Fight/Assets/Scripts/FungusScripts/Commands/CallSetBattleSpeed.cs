2ca2502324693bc115d9bc6270f1aed538f7a161
dev <dev@playtra.com>
2020-07-23 15:31:01 +0100

    Riki - Defence tutorial

---
+++ Grid Fight/Assets/Scripts/FungusScripts/Commands/CallSetBattleSpeed.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+[CommandInfo("Scripting",
+                "Call CallSetBattleSpeed",
+                "Sets the game battle speed")]
+[AddComponentMenu("")]
+public class CallSetBattleSpeed : Command
+{
+    public float Speed;
+    protected virtual void CallTheMethod()
+    {
+        BattleManagerScript.Instance.BattleSpeed = Speed;
+    }
+
+    #region Public members
+
+    public override void OnEnter()
+    {
+        CallTheMethod();
+        Continue();
+    }
+
+    public override Color GetButtonColor()
+    {
+        return new Color32(235, 191, 217, 255);
+    }
+
+    #endregion
+}
---
+++ Grid Fight/Assets/Scripts/FungusScripts/Commands/CallWaitForDefence.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+[CommandInfo("Scripting",
+                "Call CallWaitForDefence",
+                "CallWaitForDefence")]
+[AddComponentMenu("")]
+public class CallWaitForDefence : Command
+{
+
+    bool isMoving = true;
+    protected virtual void CallTheMethod()
+    {
+        BattleManagerScript.Instance.CurrentSelectedCharacters[ControllerType.Player1].Character.CurrentCharStartingActionEvent += Character_CurrentCharStartingActionEvent;
+        StartCoroutine(Defence());
+    }
+
+    private void Character_CurrentCharStartingActionEvent(ControllerType playerController, CharacterActionType action)
+    {
+        if(action == CharacterActionType.Defence)
+        {
+            isMoving = false;
+        }
+    }
+
+    IEnumerator Defence()
+    {
+        while (isMoving)
+        {
+            if(BattleManagerScript.Instance.CurrentSelectedCharacters[ControllerType.Player1].Character.isDefending)
+            {
+                BattleManagerScript.Instance.BattleSpeed = 1;
+            }
+            else
+            {
+                BattleManagerScript.Instance.BattleSpeed = 0;
+            }
+            yield return null;
+        }
+        BattleManagerScript.Instance.BattleSpeed = 1;
+        Continue();
+    }
+
+    #region Public members
+
+    public override void OnEnter()
+    {
+        CallTheMethod();
+        Continue();
+    }
+
+    public override Color GetButtonColor()
+    {
+        return new Color32(235, 191, 217, 255);
+    }
+
+    #endregion
+}
