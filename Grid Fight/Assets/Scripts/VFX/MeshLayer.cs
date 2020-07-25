ff1851d4621085d0f44ec00c98d6175d5b2e6921
devplaytra <dev@playtra.com>
2020-07-25 02:39:53 +0100

    ACE - Tutorial environment Rocks + script for a random starting rotation and size + a new shader that fakes light following a global coordinate like the center of the scene at 0,0,0

--- Grid Fight/Assets/Scripts/VFX/MeshLayer.cs
+++ Grid Fight/Assets/Scripts/VFX/MeshLayer.cs
@@ -5,9 +5,19 @@ using UnityEngine;
 public class MeshLayer : MonoBehaviour
 {
     public int Layer = 305;
-
+    public bool AllChildren = false;
     private void OnEnable()
     {
-        gameObject.GetComponent<MeshRenderer>().sortingOrder = Layer;
+        if (AllChildren)
+        {
+            foreach(MeshRenderer m in gameObject.GetComponentsInChildren<MeshRenderer>())
+            {
+                m.GetComponent<MeshRenderer>().sortingOrder = Layer;
+            }
+        }
+        else
+        {
+            gameObject.GetComponent<MeshRenderer>().sortingOrder = Layer;
+        }
     }
 }
